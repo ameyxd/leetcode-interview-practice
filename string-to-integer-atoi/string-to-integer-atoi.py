e01b410324bdac6d2e20f7422794e5825183b0a5
Amey Ambade <ameyambade@gmail.com>
2021-10-06 20:15:08 -0500

    Time: 40 ms (39.13%), Space: 14.3 MB (24.57%) - LeetHub

--- string-to-integer-atoi/string-to-integer-atoi.py
+++ string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,10 +1,8 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         strp = s.strip()
-        MAX_INT, MIN_INT = 2**31-1, -2**31
-        strp = re.search(r'^[-+]?\d+', strp)
+        strp = re.match(r'^[-+]?\d+', strp)
+        INTMAX, INTMIN = 2**31-1, -2**31
         if not strp: return 0
         num = int(strp.group())
-        # print(num)
-        return min(max(num, MIN_INT), MAX_INT)
-    
+        return min(INTMAX, max(INTMIN, num))
