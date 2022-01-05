bbabbf9f016f2102011b6e78e437a791660aa298
Amey Ambade <ameyambade@gmail.com>
2022-01-05 16:32:57 -0600

    Time: 24 ms (94.24%), Space: 14.3 MB (10.08%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -10,10 +10,10 @@ class Solution:
         :rtype: int
         """
         left, right = 1, n
-        while left < right:
+        while left <= right:
             mid = left + (right - left) // 2
             if isBadVersion(mid):
-                right = mid
+                right = mid - 1
             else:
                 left = mid + 1
         return left
