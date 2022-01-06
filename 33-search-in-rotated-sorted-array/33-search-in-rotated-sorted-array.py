dc8a27947e2e3e5ee7db3c634fc516e3e0e21b5b
Amey Ambade <ameyambade@gmail.com>
2022-01-05 18:23:03 -0600

    Time: 85 ms (5.31%), Space: 14.2 MB (99.95%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+    # def recursive_search(self, nums, left, right, target):
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[left]:
+                if nums[left] <= target and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
