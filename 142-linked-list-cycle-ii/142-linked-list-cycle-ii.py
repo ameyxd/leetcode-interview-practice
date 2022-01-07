64bf0bd94c6fe26a86cabaecbf1e7f6c419ad85a
Amey Ambade <ameyambade@gmail.com>
2022-01-07 16:41:57 -0600

    Time: 96 ms (7.48%), Space: 17.2 MB (56.12%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -8,75 +8,39 @@ class Solution:
     
     def getLength(self, slow):
         curr = slow
-        len_cycle = 0
+        cycle_length = 0
         while True:
             curr = curr.next
-            len_cycle += 1
+            cycle_length += 1
             if curr == slow:
                 break
-        return len_cycle
+        return cycle_length
     
     def detectCycle(self, head: ListNode) -> ListNode:
-        slow, fast = head, head
+        slow = head
+        fast = head
+        
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
-            if fast == slow:
-                len_cycle = self.getLength(slow)
-                break
-        else:
-            return None
-        
-        return self.findStart(head, len_cycle)
-    
-    
-    def findStart(self, head, len_cycle):
-        ptr1, ptr2 = head, head
-        while len_cycle:
-            ptr1 = ptr1.next
-            len_cycle -= 1
-        while ptr1 != ptr2:
-            ptr1 = ptr1.next
-            ptr2 = ptr2.next
-        
-        return ptr1
-
-    
-#     def getLength(self, slow):
-#         curr = slow
-#         cycle_length = 0
-#         while True:
-#             curr = curr.next
-#             cycle_length += 1
-#             if curr == slow:
-#                 break
-#         return cycle_length
-    
-#     def detectCycle(self, head: ListNode) -> ListNode:
-#         slow = head
-#         fast = head
-        
-#         while fast and fast.next:
-#             fast = fast.next.next
-#             slow = slow.next
             
-#             if fast == slow:
-#                 cycle_length = self.getLength(slow)
-#                 print(cycle_length)
-#                 break
-#         else: # Important - while-else clause in case there is no cycle
-#             return None
-#         return self.findStart(head, cycle_length)
+            if fast == slow:
+                cycle_length = self.getLength(slow)
+                print(cycle_length)
+                break
+        else: # Important - while-else clause in case there is no cycle
+            return None
+        return self.findStart(head, cycle_length)
         
-#     def findStart(self, head, cycle_length):
-#         ptr1, ptr2 = head, head
-#         while cycle_length:
-#             ptr2 = ptr2.next
-#             cycle_length -= 1
-#         while ptr1 != ptr2:
-#             ptr2 = ptr2.next
-#             ptr1 = ptr1.next
-#         return ptr1
+    def findStart(self, head, cycle_length):
+        ptr1, ptr2 = head, head
+        while cycle_length:
+            ptr2 = ptr2.next
+            cycle_length -= 1
+        while ptr1 != ptr2:
+            ptr2 = ptr2.next
+            ptr1 = ptr1.next
+        return ptr1
 
     
     ## Strat: find where the fast and slow pointers meet, if start of cycle is at pos m, and fast and slow meet at k steps from m:
