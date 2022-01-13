3592dcf4a9e66f4fe8b2897e6d8b49587b97a616
Amey Ambade <ameyambade@gmail.com>
2022-01-12 22:44:39 -0600

    Time: 367 ms (15.95%), Space: 21.1 MB (81.43%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,17 +1,32 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix = [1] * len(nums)
-        postfix = [1] * len(nums)
+        
+#         # Calculate leftProduct and rightProduct arrays to keep track of product until before and after any point i, then multiply them together
+        
+#         prefix = [1] * len(nums)
+#         postfix = [1] * len(nums)
                 
-        for i in range(1, len(nums)):
-            prefix[i] = nums[i - 1] * prefix[i - 1]
+#         for i in range(1, len(nums)):
+#             prefix[i] = nums[i - 1] * prefix[i - 1]
         
-        for i in range(len(nums) - 2, -1, -1):
-            postfix[i] = postfix[i + 1] * nums[i + 1]
+#         for i in range(len(nums) - 2, -1, -1):
+#             postfix[i] = postfix[i + 1] * nums[i + 1]
         
-        answer = [1] * len(nums)
+#         answer = [1] * len(nums)
+        
+#         for i in range(len(nums)):
+#             answer[i] = prefix[i] * postfix[i]
+        
+#         return answer
+
+#         # O(1) space solution:
+        prefix, postfix = 1, 1
+        ans = [1] * len(nums)
         
         for i in range(len(nums)):
-            answer[i] = prefix[i] * postfix[i]
-        
-        return answer
+            ans[i] = prefix
+            prefix *= nums[i]
+        for i in range(len(nums) - 1, -1, -1):
+            ans[i] *= postfix
+            postfix *= nums[i]
+        return ans
