87aee2faebc97455318285a06cbd64178af6b047
Amey Ambade <ameyambade@gmail.com>
2022-01-12 22:01:55 -0600

    Time: 1231 ms (12.15%), Space: 28.8 MB (33.11%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -3,6 +3,8 @@ class Solution:
         maxSum = nums[0]
         currSum = 0
         
+        # If the number you're looking at is greater than the sum of everything up to it, i.e. (current sum + n), then set the current sum to n, else add n to current ssum
+        
         for n in nums:
             if n > currSum + n:
                 currSum = n
