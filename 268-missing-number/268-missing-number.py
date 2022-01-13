2a1c3740cbe1475e34831e7b68a18863885724ec
Amey Ambade <ameyambade@gmail.com>
2022-01-13 15:44:00 -0600

    Time: 266 ms (17.97%), Space: 15.3 MB (79.93%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,5 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        res = n*(n+1)//2 - sum(nums)
-        return res
+        # # Sum everything in range and subtract original array's sum
+        # n = len(nums)
+        # res = n*(n+1)//2 - sum(nums)
+        # return res
+        
+        # XOR Solution: We know XOR of number with itself is 0 and XOR of 0 with any number is the same number. So XOR the input list with the range in a loop
+        
+        xor_res = len(nums)
+        for i, num in enumerate(nums):
+            xor_res ^= i ^ num # res ^ i ^ i'th value
+        
+        return xor_res
