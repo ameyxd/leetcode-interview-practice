5cab04dbda95ba70eda99e4a9fd18a0c4ad3a68a
Amey Ambade <ameyambade@gmail.com>
2022-01-13 21:14:29 -0600

    Time: 849 ms (76.18%), Space: 17.6 MB (49.04%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,13 +1,18 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        # Strat: Sort list, pick a one by one, and apply left-right pointer technique from Two Sum II to get theb and c that sum to 0
+        
         nums = sorted(nums)
         res = []
         
         for i, a in enumerate(nums):
+            # Handle duplicates in first element (a)
             if i > 0 and a == nums[i - 1]:
                 continue
-            # if a > 0:
-            #     return res
+            # Optimization: If first num of the three in sorted list is already positive then sum cannot be zero so return result
+            if a > 0:
+                return res
             l, r = i + 1, len(nums) - 1
             while l < r:
                 threeSum = nums[l] + nums[r] + a
@@ -18,6 +23,10 @@ class Solution:
                 else:
                     res.append([a, nums[l], nums[r]])
                     l += 1
+                    # Handle duplicates
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
         return res
+    
+    
+        # Time complexity: O(nlogn) + O(n^2) = o(n^2)
