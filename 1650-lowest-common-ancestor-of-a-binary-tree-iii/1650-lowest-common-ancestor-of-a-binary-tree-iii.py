846a97be6623e034a885e83a64ac261bce5e1867
Amey Ambade <ameyambade@gmail.com>
2022-01-28 13:18:21 -0600

    Time: 76 ms (53.62%), Space: 18.4 MB (86.46%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -10,20 +10,6 @@ class Node:
 
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-#         stack = [root]
-        
-#         parent_dict = {root: None}
-        
-#         while p not in parent_dict or q not in parent_dict:
-#             if not stack:
-#                 return None
-#             node = stack.pop()
-#             if node.left:
-#                 parent_dict[node.left] = node
-#                 stack.append(node.left)
-#             if node.right:
-#                 parent_dict[node.right] = node
-#                 stack.append(node.right)
 
         # Make ancestor list of p        
         ancestors = set()
