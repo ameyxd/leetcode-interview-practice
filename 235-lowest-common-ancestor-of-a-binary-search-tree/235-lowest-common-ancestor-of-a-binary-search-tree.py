7a36186f9c5f27807091a9ae66df0840d44988de
Amey Ambade <ameyambade@gmail.com>
2022-01-27 22:04:29 -0600

    Time: 76 ms (83.51%), Space: 18.7 MB (10.84%) - LeetHub

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # Strat: LCA will be either where the split occurred, i.e., p is on left of node and q is on right of node, or if we reach p or q, i.e., p is ancestor to itself and q or q is ancestor it p or itself
+        curr = root
+        while curr:    # Essentiallty we want to keep going until we guaranteed return
+            if p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            else: # We have got curr to the point where p < curr < q or if curr == p or if curr == q
+                return curr
+        
+                
