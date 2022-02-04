7330c040b1185f24adc333ef16416c836eebd9db
Amey Ambade <ameyambade@gmail.com>
2022-02-03 20:57:54 -0600

    Time: 61 ms (53.05%), Space: 18 MB (84.85%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -5,14 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # Recursive solution: Get inorder result -> sorted array and return k - 1 th element
     def inorder(self, root):
         if root:
             self.inorder(root.left)
             self.sorted_elements.append(root.val)
             self.inorder(root.right)
         
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+    def kthSmallest_1(self, root: Optional[TreeNode], k: int) -> int:
         self.sorted_elements = []
         self.inorder(root)
         print(self.sorted_elements)
         return self.sorted_elements[k - 1]
+    
+    # Iterative solution
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        curr = root
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            node = stack.pop()
+            k -= 1
+            if k == 0:
+                return node.val
+            curr = node.right
+        
