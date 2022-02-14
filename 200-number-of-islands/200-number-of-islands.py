f3a75bc2ad2c00aa245829741fd79ab0f15be65f
Amey Ambade <ameyambade@gmail.com>
2022-02-13 22:07:59 -0600

    Time: 490 ms (34.87%), Space: 16.6 MB (69.90%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,5 +1,7 @@
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+
+    # BFS Solution
+    def numIslands_1(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
         rows, cols = len(grid), len(grid[0])
@@ -21,12 +23,35 @@ class Solution:
                         q.append((new_row, new_col))
                         visited.add((new_row, new_col))
                 
-            
         
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == "1" and (row, col) not in visited:
                     bfs(row, col)
+                    islands += 1
+                    
+        return islands
+    
+    # DFS Solution
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        rows, cols = len(grid), len(grid[0])
+        islands = 0
+        
+        def dfs(r, c):
+            if r not in range(rows) or c not in range(cols) or grid[r][c] != "1":
+                return
+            grid[r][c] = "#"
+            dfs(r - 1, c)
+            dfs(r + 1, c)
+            dfs(r, c - 1)
+            dfs(r, c + 1)
+        
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == "1":
+                    dfs(row, col)
                     islands += 1
                     
         return islands
