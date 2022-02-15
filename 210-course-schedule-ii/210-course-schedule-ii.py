aef385a874b58582076ed89cd2b693cd455f5d76
Amey Ambade <ameyambade@gmail.com>
2022-02-15 15:18:22 -0600

    Time: 104 ms (65.36%), Space: 15.4 MB (87.96%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # Using topological sort: start with nodes with 0 indegree, add to queue and process neighbors by reducing their indegrees by 1
+        
+        adj = {i: [] for i in range(numCourses)}
+        indegree = {}
+        for course, prereq in prerequisites:
+            indegree[course] = indegree.get(course, 0) + 1
+            adj[prereq].append(course)
+            
+        queue = collections.deque([k for k in range(numCourses) if k not in indegree])
+        top_sort_res = []
+        
+        while queue:
+            course = queue.popleft()
+            top_sort_res.append(course)
+            for prereq in adj[course]:
+                indegree[prereq] -= 1
+                if indegree[prereq] == 0:
+                    queue.append(prereq)
+
+        return top_sort_res if len(top_sort_res) == numCourses else [] #Else there is a cycle
