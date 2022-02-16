e99c85391bd4daa2f66fbb2538c499d4095bce57
Amey Ambade <ameyambade@gmail.com>
2022-02-15 20:01:59 -0600

    Time: 178 ms (27.59%), Space: 17.2 MB (21.61%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,14 +1,21 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         
-        adj = {i: [] for i in range(n)}
-        visited = set()
+        # Run recursive DFS from any node, every time a new non-recursive call starts, that means that node was not reachable from any other node seen
         
+        # initialize graph adjacency list
+        adj = {i: [] for i in range(n)}
+        
+        # Set to keep track of which nodes are visited
+        visited = set()
+
+        # Build graph
         for edge in edges:
             n1, n2 = edge[0], edge[1]
             adj[n1].append(n2)
             adj[n2].append(n1)
         
+        # Recursive dfs while neighbor is not visited
         def dfs(node):
             visited.add(node)
             for nei in adj[node]:
@@ -17,6 +24,7 @@ class Solution:
         
         components = 0
         
+        # Run dfs on all nodes, every time a dfs call runs from this loop, it means that node was not reachable from other nodes, so that is the start of a new connected component
         for i in range(n):
             if i not in visited:
                 components += 1
