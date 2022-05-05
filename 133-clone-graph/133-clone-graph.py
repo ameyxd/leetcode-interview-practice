9cb2da36dd7f2b7c4852db4ac1435708f2b7088a
Amey Ambade <ameyambade@gmail.com>
2022-05-04 19:59:58 -0500

    Time: 43 ms (74.66%), Space: 14.3 MB (79.42%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -10,13 +10,18 @@ class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
        # Recursive DFS with hashmap from old node to copy (new node)
         oldToNew = {}
-        
+
+        # DFS function returns the copy of the node it is passed
         def dfs(node):
+            # if node is already cloned, get it from hashmap
             if node in oldToNew:
                 return oldToNew[node]
+            # else create clone and add to hashmap
             copy = Node(node.val)
             oldToNew[node] = copy
+            
+            # Run dfs recursively on each of the neighbors and add them to neighbors of copy
             for neighbor in node.neighbors:
-                copy.neighbors.append(dfs(neighbor))
+                copy.neighbors.append(dfs(neighbor)) # remember that we are appending the (returned copy node of neighbor from dfs function) to the copied node
             return copy
         return dfs(node) if node else None
