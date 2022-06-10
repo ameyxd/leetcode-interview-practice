5c647f343385cc408ebf2e80509d03ea8825cf27
Amey Ambade <ameyambade@gmail.com>
2022-06-10 18:55:55 -0500

    Time: 85 ms (25.01%), Space: 14.9 MB (6.09%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,6 +1,6 @@
 class Solution:
     # Same as binary search, catch is to return left pointer if element is not found
-    def searchInsert(self, nums: List[int], target: int) -> int:
+    def searchInsert1(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
         while left <= right:
             pivot = left + (right - left) // 2
@@ -12,3 +12,14 @@ class Solution:
                 left = pivot + 1
         return left
                 
+        
+        # Better implementation like template
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) # Since search space needs to be one larger if target is greater than last element it will have to go in the end acc to question
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] >= target:
+                right = mid
+            else:
+                left = mid + 1
+        return left
