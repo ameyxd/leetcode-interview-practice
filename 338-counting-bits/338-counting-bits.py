6368aae03acc02e856cf217809f0a96f26d24ac3
Amey Ambade <ameyambade@gmail.com>
2022-06-12 13:31:30 -0500

    Time: 116 ms (62.49%), Space: 20.9 MB (27.94%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countBits1(self, n: int) -> List[int]:
+        # Using bit counting for each element
+        def hammingWeight(x):
+            count = 0
+            while x > 0:
+                if x & 1:
+                    count += 1
+                x >>= 1
+            return count
+        
+        return [hammingWeight(i) for i in range(n + 1)]
+    
+    # DP Solution using the least significant bit -> Ans(x) = Ans(x/2) + (x % 2) [which is the last significant digit]
+    
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+        for x in range(0, n + 1):
+            ans[x] = ans[x >> 1] + (x & 1) # x >> 1 is x/2 and x&1 is the same as x%2 i.e. least significant bit
+        return ans
