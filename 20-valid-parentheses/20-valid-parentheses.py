a16d5a66dec5150cb671c4b46377fb84fa00b061
Amey Ambade <ameyambade@gmail.com>
2022-06-12 13:01:23 -0500

    Time: 87 ms (5.25%), Space: 14 MB (22.97%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,16 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        d = {"}": "{", ")" : "(", "]" : "["}
+        d = {'}': '{', ']': '[', ')':'('}
         
         stack = []
         for bracket in s:
-            # if it is an open bracket, put into stack
-            if bracket in d.values():
+            if bracket in d.values(): #opening bracket
                 stack.append(bracket)
-            # If it is a closed bracket, pop from stack and compare
-            elif bracket in d.keys():
-                curr_open_bracket = stack.pop() if stack else "$"
-                if curr_open_bracket != d[bracket]:
+            elif bracket in d.keys(): # If it is a closed bracket, pop from stack and compare
+                curr_open = stack.pop() if stack else "%"
+                if d[bracket] != curr_open:
                     return False
         if len(stack) == 0:
             return True
