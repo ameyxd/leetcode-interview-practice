72160602072d0586675af0762f425f3704b40aaa
Amey Ambade <ameyambade@gmail.com>
2022-06-12 12:54:58 -0500

    Time: 466 ms (9.15%), Space: 15.5 MB (70.44%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -11,14 +11,3 @@ class Solution:
                 left = mid + 1
         return -1
     
-#         left, right = 0, len(nums) - 1
-#         while left <= right:
-#             pivot = left + (right - left) // 2
-#             if nums[pivot] == target:
-#                 return pivot
-#             elif nums[pivot] > target:
-#                 right = pivot - 1
-#             else:
-#                 left = pivot + 1
-#         return -1
-        
