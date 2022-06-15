bc01f46417833ee7f1a464467f97e2928111ad36
Amey Ambade <ameyambade@gmail.com>
2022-06-15 11:40:51 -0500

    Time: 156 ms (85.06%), Space: 18.9 MB (9.59%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,9 +1,9 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         # Sort intervals by start value
-        intervals.sort(key = lambda i : i[0])
+        intervals.sort(key = lambda i : i[0]) # in python if you just use sort it will default to using the first value anyways so you can use intervals.sort()
         output = [intervals[0]]
-        # Check if tehre is overlap in consecutive intervals
+        # Check if there is overlap in consecutive intervals
         for start, end in intervals[1:]:
             lastEnd = output[-1][1]
             # Merge if overlap
