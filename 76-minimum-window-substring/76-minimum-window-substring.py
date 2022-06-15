6b162b3db6daf878d872c3fefc3bb3f669607b7c
Amey Ambade <ameyambade@gmail.com>
2022-06-15 07:34:41 -0500

    Time: 170 ms (46.09%), Space: 14.7 MB (82.60%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -69,4 +69,4 @@ class Solution(object):
                     have -= 1
                 l += 1
         l, r = res
-        return s[l: r + 1] if resLen != float("infinity") else ""
+        return s[l: r + 1]# if resLen != float("infinity") else ""
