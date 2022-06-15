dd6271cd30ada1adddf5ab0fd6fd116aa2425cf0
Amey Ambade <ameyambade@gmail.com>
2022-06-15 07:10:24 -0500

    Time: 303 ms (18.64%), Space: 14 MB (18.96%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -0,0 +1,22 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # sliding window - slide l and r anytime substring becomes invalid, i.e., no of possible replacements in the string > k
+        
+        l = 0
+        count = {}
+        res = 0 # length of longest window
+        
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            substringLen = r - l + 1
+            mostFreqCount = max(count.values())
+            
+            # shift l to the right if invalid substring
+            if substringLen - mostFreqCount > k: # understand why this is correct - this condition is the starting point for code and intuition 
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)   
+                
+        return res
+    
+    # O(26*n)
