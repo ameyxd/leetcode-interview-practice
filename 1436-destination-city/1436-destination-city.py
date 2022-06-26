6f8df43a6ccddef8131b6b9043e311c61f0db509
Amey Ambade <ameyambade@gmail.com>
2022-06-26 15:44:50 -0500

    Time: 89 ms (39.14%), Space: 13.9 MB (79.81%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -1,11 +1,21 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        # City with no outgoing nodes is answer
-        outgoing_count = {}
+        
+        city_map = {}
         for cityA, cityB in paths:
-            outgoing_count[cityA] = outgoing_count.get(cityA, 0) + 1
-            outgoing_count[cityB] = outgoing_count.get(cityB, 0)
-            
-        for city in outgoing_count:
-            if outgoing_count[city] == 0:
+            city_map[cityA] = cityB
+        
+        for city in city_map.values():
+            if city not in city_map.keys():
                 return city
+        
+        # City with no outgoing nodes is answer
+        # O(n)
+#         outgoing_count = {}
+#         for cityA, cityB in paths:
+#             outgoing_count[cityA] = outgoing_count.get(cityA, 0) + 1
+#             outgoing_count[cityB] = outgoing_count.get(cityB, 0)
+            
+#         for city in outgoing_count:
+#             if outgoing_count[city] == 0:
+#                 return city
