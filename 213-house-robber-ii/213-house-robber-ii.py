75903d5b4cc9c75a01518e97d8008eb9254fb5a5
Amey Ambade <ameyambade@gmail.com>
2022-06-28 16:45:34 -0500

    Time: 38 ms (79.93%), Space: 13.9 MB (23.91%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # Problem breaks down into solving house robbers and getting max of stealing from house 2 to n, and from 1 to n - 1
         def helper(nums): # This helper function is the solution of house robber 1
             rob1, rob2 = 0, 0
             for house in nums:
@@ -8,4 +9,4 @@ class Solution:
                 rob2 = currRob
             return rob2
     
-        return max(nums[0], helper(nums[1:]), helper(nums[:-1]))
+        return max(nums[0], helper(nums[1:]), helper(nums[:-1])) # nums[0] for when the input is only one element. 
