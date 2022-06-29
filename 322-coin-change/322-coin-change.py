3c6785bf175a8d8724da2e5a6bd3291ba79fbe93
Amey Ambade <ameyambade@gmail.com>
2022-06-28 21:35:24 -0500

    Time: 1609 ms (77.88%), Space: 14.3 MB (40.64%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,16 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # Brute force - dfs backtracking: n choices in decision tree
+        # Repeating subproblems - make tree diagram
+        MAX_INT = float("inf") # You can use amount + 1 instead of float inf
+        dp = [MAX_INT] * (amount + 1) 
+        dp[0] = 0 # min number of coins to sum to 0
+        # dp[1] = # min number of coins to sum to 1
+        
+        for a in range(1, amount + 1):
+            for coin in coins:
+                if a - coin >= 0:
+                    # We can continue searching
+                    dp[a] = min(dp[a], 1 + dp[a - coin]) # eg coin = 4, a = 7, then dp[7] = 1 + dp[7 - 4]
+        
+        return dp[amount] if dp[amount] != MAX_INT else -1
