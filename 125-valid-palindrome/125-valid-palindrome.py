3d48e9fa7c4342c87b8884d4bfcbfd7b5ef71e65
Amey Ambade <ameyambade@gmail.com>
2022-06-30 11:46:09 -0500

    Time: 71 ms (53.15%), Space: 15.3 MB (26.12%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,9 +1,6 @@
-import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-#         edited_str = ''.join(char for char in s.lower() if char.isalnum())
-#         return edited_str == edited_str[::-1]
-    
-        # Regex solution
-        edited_str = re.sub('[^a-zA-Z0-9]', '', s).lower()
-        return edited_str == edited_str[::-1]
+        raw_str = ''.join([char for char in s.lower() if char.isalnum()])
+        # re.sub('^[a-zA-Z0-9]', '', s).lower() # Regex solution
+
+        return raw_str == raw_str[::-1]
