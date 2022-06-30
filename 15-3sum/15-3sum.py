30d7be98b4ab9135a10a32dff72bb443624afe7f
Amey Ambade <ameyambade@gmail.com>
2022-06-30 14:32:38 -0500

    Time: 819 ms (84.46%), Space: 18.1 MB (41.77%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,31 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # Strat: Sort list, pick a one by one, and apply left-right pointer technique from Two Sum II to get theb and c that sum to 0
-        
-        nums = sorted(nums)
         res = []
-        
-        for i, a in enumerate(nums):
-            # Handle duplicates in first element (a)
-            if i > 0 and a == nums[i - 1]:
+        nums = sorted(nums)
+        for i in range(len(nums)):
+            # handle duplicates in start
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            # Optimization: If first num of the three in sorted list is already positive then sum cannot be zero so return result
-            if a > 0:
+            
+            # Optimization: if the first num of the three in sorted array is positive, return result since no combination after that will sum to 0
+            if nums[i] > 0:
                 return res
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                threeSum = nums[l] + nums[r] + a
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
+            
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                threesum = nums[i] + nums[j] + nums[k]
+                if threesum == 0:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    
+                    # Handle duplicates when j moves
+                    while nums[j] == nums[j - 1] and j < k:
+                        j += 1
+                    
+                elif threesum > 0:
+                    k -= 1
                 else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
-                    # Handle duplicates
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
+                    j += 1
         return res
-    
-    
-        # Time complexity: O(nlogn) + O(n^2) = o(n^2)
