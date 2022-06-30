92d1a459a458f7cf38ff32d5c458db6cb98bd99b
Amey Ambade <ameyambade@gmail.com>
2022-06-30 10:47:19 -0500

    Time: 222 ms (11.58%), Space: 19.7 MB (6.18%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,5 +1,5 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent1(self, nums: List[int], k: int) -> List[int]:
         # O(n log k): Minheap of size k
         if k == len(nums):
             return nums
@@ -28,3 +28,19 @@ class Solution:
         for _ in range(k):
             res.append(heapq.heappop(heap)[1])
         return res
+
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # O(n): Bucket sort like - need values to be in a range as a condition on input
+        # Make hashmap of frequency, then make a list, where at index i store the value that occurs i times
+        if k == len(nums):
+            return nums
+    
+        countDict = Counter(nums)
+        heap, res = [], []
+        bucket_list = [[] for _ in range(len(nums) + 1)]
+        for num, count in countDict.items():
+            bucket_list[count].append(num)
+        flattened_list = [item for sublist in bucket_list for item in sublist]
+        # Go in reverse order since most frequent element will be last
+        return flattened_list[::-1][:k]
+    
