c505f8000db354fbb9d0629795c4e0bb9f8eb8bf
Amey Ambade <ameyambade@gmail.com>
2022-06-30 11:01:51 -0500

    Time: 409 ms (25.75%), Space: 23.4 MB (7.38%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,32 +1,31 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix, postfix = [1] * len(nums), [1] * len(nums)
         
-#         # Calculate leftProduct and rightProduct arrays to keep track of product until before and after any point i, then multiply them together
+        for i in range(1, len(nums)):
+            prefix[i] = nums[i - 1] * prefix[i - 1]
         
-#         prefix = [1] * len(nums)
-#         postfix = [1] * len(nums)
-                
-#         for i in range(1, len(nums)):
-#             prefix[i] = nums[i - 1] * prefix[i - 1]
+        for i in range(len(nums) - 2, -1, -1):
+            postfix[i] = nums[i + 1] * postfix[i + 1]
         
-#         for i in range(len(nums) - 2, -1, -1):
-#             postfix[i] = postfix[i + 1] * nums[i + 1]
-        
-#         answer = [1] * len(nums)
-        
-#         for i in range(len(nums)):
-#             answer[i] = prefix[i] * postfix[i]
-        
-#         return answer
-
-#         # O(1) space solution:
-        prefix, postfix = 1, 1
-        ans = [1] * len(nums)
+        answer = [1] * len(nums)
         
         for i in range(len(nums)):
-            ans[i] = prefix
+            answer[i] = prefix[i] * postfix[i]
+        
+        return answer
+    
+    def productExceptSelf1(self, nums: List[int]) -> List[int]:
+    # O(1) space solution
+        answer = [1] * len(nums)
+        prefix, postfix = 1, 1
+        
+        # Make answer array have prefixes first
+        for i in range(len(nums)):
+            answer[i] = prefix
             prefix *= nums[i]
+        # Then go through answer array in reverse and multiply the postfix
         for i in range(len(nums) - 1, -1, -1):
-            ans[i] *= postfix
+            answer[i] *= postfix
             postfix *= nums[i]
-        return ans
+        return answer
