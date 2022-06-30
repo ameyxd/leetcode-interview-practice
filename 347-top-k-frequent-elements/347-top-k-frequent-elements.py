41bb4d5905f629f1bcb549a7daaf2f888c671f0f
Amey Ambade <ameyambade@gmail.com>
2022-06-29 21:55:52 -0500

    Time: 215 ms (13.62%), Space: 18.9 MB (33.76%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,5 +1,5 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent1(self, nums: List[int], k: int) -> List[int]:
         # O(n log k): Minheap of size k
         if k == len(nums):
             return nums
@@ -15,14 +15,14 @@ class Solution:
         #     res.append(heapq.heappop(heap)[1])
         # return res
     
-    def topKFrequent1(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # O(k log n): Maxheap of size n
         if k == len(nums):
             return nums
     
         countDict = Counter(nums)
         heap, res = [], []
-        for count, num in countDict.items():
+        for num, count in countDict.items():
             heapq.heappush(heap, (-count, num))
         
         for _ in range(k):
