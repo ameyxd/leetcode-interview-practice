9d5725de197bf119bde310c1837a47ec0b677fcd
Amey Ambade <ameyambade@gmail.com>
2022-07-05 17:44:51 -0500

    Time: 99 ms (8.01%), Space: 16.9 MB (25.25%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -5,32 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # SOLUTION 1: Check if list from inorder traversal is sorted or not
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        result = []
-        self.inorderCheck(root, result)
-        for i in range(len(result)-1):
-            if result[i+1] <= result[i]:
-                return False
-        return True
-        
-    def inorderCheck(self, root, inorder_list):
-        if root:
-            self.inorderCheck(root.left, inorder_list)
-            inorder_list.append(root.val)
-            self.inorderCheck(root.right, inorder_list)
-        
-    # SOLUTION 1: Check if root vals are greater than all vals in left subtree and less than all vals in right subtree
+    
     def check(self, root, leftMax, rightMin):
         if not root:
             return True
-        l = self.check(root.left, leftMax, root.val)
-        v = (leftMax < root.val < rightMin)
-        r = self.check(root.right, root.val, rightMin)
-        return l and v and r
-
+        leftRes = self.check(root.left, leftMax, root.val)
+        rightRes = self.check(root.right, root.val, rightMin)
+        valCheck = (leftMax < root.val < rightMin)
+        
+        return leftRes and rightRes and valCheck
     
-    def isValidBST_1(self, root: Optional[TreeNode]) -> bool:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # check if root is greater than left subtree nodes and less than right subtree nodes
+        # keep track of max value in left subtree and min val in right subtree
+
         return self.check(root, float("-inf"), float("inf"))
         
-
