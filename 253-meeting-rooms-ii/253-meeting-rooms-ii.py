fc2c9d938bd8e56e1bdc90e28e9811c724c0ee83
Amey Ambade <ameyambade@gmail.com>
2022-07-18 20:17:05 -0500

    Time: 122 ms (48.80%), Space: 17.6 MB (49.16%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,34 +1,33 @@
 class Solution:
     def minMeetingRooms1(self, intervals: List[List[int]]) -> int:
-        # Min number of conference rooms required = maximum number of overlapping intervals
+        # make two arrays for start and end times use two pointers to traverse start and end arrays
         start = sorted([i[0] for i in intervals])
         end = sorted([i[1] for i in intervals])
-        print(start, end)
-        res, count = 0, 0
-        # Two pointers to traverse through start and end array
+        count, res = 0, 0
         s, e = 0, 0
-        while s < len(intervals): # while loop because we only need to worry about meetings starting to keep count of # conference rooms required
-            if start[s] < end[e]: # first meeting begins before another ends
+        while s < len(intervals):
+            # New room required
+            if start[s] < end[e]:
                 s += 1
                 count += 1
+            
+            # Room emptied, meeting complete
             else:
                 e += 1
-                count -= 1 # A meeting ended before another could start -> reduce # conference rooms needed
-            res = max(res, count)
+                count -= 1
+            res = max(count, res)
         return res
     
+    # Min heap
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # Another approach: min-heap that helps keep track of when the earliest meeting ends
-        if not intervals:
-            return 0
-        intervals.sort(key = lambda i: i[0])
-        free_rooms = []
-        
-        heapq.heappush(free_rooms, intervals[0][1])
+        intervals.sort(key=lambda i: i[0])
+        endTimeHeap = []
+        heapq.heappush(endTimeHeap, intervals[0][1])
         
         for i in intervals[1:]:
-            if free_rooms[0] <= i[0]: # if the earliest ending meeting is ending before current event starts
-                heapq.heappop(free_rooms) # assign to current meeting
-            heapq.heappush(free_rooms, i[1]) # add current room's end time to heap
-            
-        return len(free_rooms)
+            # of all occupied rooms, if the earliest ending meeting ends before current meeting starts
+            if endTimeHeap[0] <= i[0]:
+                heapq.heappop(endTimeHeap)
+            heapq.heappush(endTimeHeap, i[1])
+        
+        return len(endTimeHeap)
