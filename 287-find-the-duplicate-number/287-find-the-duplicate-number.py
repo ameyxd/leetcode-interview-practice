3a8087c720569ea6eefe37be13db15cfab0c1c73
Amey Ambade <ameyambade@gmail.com>
2022-07-26 15:18:12 -0500

    Time: 699 ms (85.54%), Space: 28 MB (54.79%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
+    def findDuplicate_marknegative(self, nums: List[int]) -> int:
 #         [1, 3, 4, 2, 2]
 #         ITERATE THROUGH THE list and for every number that i see, i set that i to -i that corresponding element
 #         if at any point, the number that is to be marked is already marked return that i
@@ -16,3 +16,18 @@ class Solution:
         for i in range(len(nums)):
             nums[i] = abs(nums[i])
         return cur
+    
+    def findDuplicate(self, nums: List[int]) -> int:
+        fast = slow = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if fast == slow:
+                break
+
+        p1 = nums[0]
+        while p1 != slow:
+            p1 = nums[p1]
+            slow = nums[slow]
+        
+        return slow
