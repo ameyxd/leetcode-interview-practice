a087d313f8dec5bba473a1d302375547e8c3a0fc
Amey Ambade <ameyambade@gmail.com>
2022-07-28 18:29:50 -0500

    Time: 125 ms (98.39%), Space: 17.6 MB (38.20%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.py
@@ -0,0 +1,30 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # Levelwise BFS: it matters how you build the adj list
+        if endWord not in wordList:
+            return 0
+        # Adj list stores a pattern mapped to the words that match that pattern O()
+        adj = collections.defaultdict(list)
+        wordList.append(beginWord)
+        for word in wordList:
+            for pos in range(len(word)):
+                pattern = word[:pos] + "*" + word[pos + 1:] 
+                adj[pattern].append(word)
+                
+        visited = set([beginWord])
+        queue = deque([beginWord])
+        res = 1
+        
+        while queue:
+            for i in range(len(queue)):
+                word = queue.popleft()
+                if word == endWord:
+                    return res
+                for pos in range(len(word)):
+                    pattern = word[:pos] + "*" + word[pos + 1:]
+                    for nei in adj[pattern]:
+                        if nei not in visited:
+                            queue.append(nei)
+                            visited.add(nei)
+            res += 1            
+        return 0
