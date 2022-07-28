d5c5346348b9b0ca16fa61da9f47336009acf5b9
Amey Ambade <ameyambade@gmail.com>
2022-07-28 00:00:37 -0500

    Time: 68 ms (70.81%), Space: 14 MB (10.11%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,16 +1,14 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        # BFS
+        # BFS - Level order traversal
         if not grid:
             return -1
-
         rows, cols = len(grid), len(grid[0])
         steps = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        
-        queue, time = collections.deque(), 0
+        queue = deque()
         minutes, fresh = 0, 0
         
-        
+        # populate the queue initially and count the number of fresh oranges
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2:
@@ -18,21 +16,20 @@ class Solution:
                 elif grid[r][c] == 1:
                     fresh += 1
         
-        if not fresh: # the oranges are already all rotten
+        if not fresh: # all oranges are already rotten
             return 0
         
         while queue:
             minutes += 1
-            
             for i in range(len(queue)):
                 r, c = queue.popleft()
                 for dr, dc in steps:
-                    newr, newc = r + dr, c + dc
-                    if newr in range(rows) and newc in range(cols) and grid[newr][newc] == 1:
-                        grid[newr][newc] = 2
+                    nr, nc = r + dr, c + dc
+                    if nr in range(rows) and nc in range(cols) and grid[nr][nc] == 1:
+                        # Rot the orange
+                        grid[nr][nc] = 2
+                        queue.append((nr, nc))
                         fresh -= 1
-                        queue.append((newr, newc))
-
-        
+                        
         return minutes - 1 if not fresh else -1
-        # Minutes - 1 because the last while loop will try to find neighbors of the last rotten orange, but since that doesn't affect any other oranges, we shouldn't include that round
+        # return minutes - 1 because the last while loop will try to find neighbors of the last rotten orange, but since that doesn't affect any other oranges, we shouldn't include that round
