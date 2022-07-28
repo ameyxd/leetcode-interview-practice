64195ce8a56150bec88529dbc4168fa8e48b02d3
Amey Ambade <ameyambade@gmail.com>
2022-07-28 15:10:15 -0500

    Time: 441 ms (46.28%), Space: 16.8 MB (85.39%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.py
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -7,6 +7,7 @@ class Solution:
         steps = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         
         # start from gates, BFS to get to each node that is empty. Current node's value will be the node on the prev path's value + 1
+        # Populate queue and see how each gate's is not fully explored before moving on to the next gate, since each gate only looks at areas within one space of it. So these one space away rooms are marked and added to queue, and so on. Once all gates are checked, each new space in the queue is checked, and so on, so when a room is reached, it has to be from the closest gate. This guarantees that we get the distance from the closest gate.
         queue = deque()
         for r in range(ROWS):
             for c in range(COLS):
