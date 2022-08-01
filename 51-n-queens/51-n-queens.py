9e57cfdbdb5f5622d8ed09581b533835549e2ab5
Amey Ambade <ameyambade@gmail.com>
2022-08-01 15:21:26 -0500

    Time: 112 ms (40.89%), Space: 14.4 MB (82.60%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -2,42 +2,37 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         ROWS, COLS = n, n
         col = set()
-        posDiag = set() # (r + c)
-        negDiag = set() # (r - c)
+        posDiag = set() # store (r + c)
+        negDiag = set() # store (r - c)
         res = []
-        board = [["."] * n for i in range(n)]
+        board = [["."] * COLS for i in range(ROWS)] 
         
-        
-        
-        # Index of number in colPlacements tells us the row
-        res = []
-                
         def backtrack(r):
-            # base case: goal
+            # base case: goal reached
             if r == ROWS:
+                # append board to result
                 copy = ["".join(row) for row in board]
                 res.append(copy)
                 return
             else:
                 for c in range(COLS):
                     # Validate
-                    if c in col or (r + c) in posDiag or (r - c) in negDiag: # Skip, you cannot use this position
+                    if c in col or (r + c) in posDiag or (r - c) in negDiag:
                         continue
-                    # Choose
+                    # Place
                     col.add(c)
                     posDiag.add(r + c)
                     negDiag.add(r - c)
                     board[r][c] = "Q"
-                    
-                    # Explore
+
+                    # Backtrack recurse
                     backtrack(r + 1)
 
-                    # Unchoose
+                    # Unplace
                     col.remove(c)
                     posDiag.remove(r + c)
                     negDiag.remove(r - c)
                     board[r][c] = "."
-        
+            
         backtrack(0)
-        
         return res
