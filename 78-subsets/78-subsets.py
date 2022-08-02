5e3bdf6fb304f613acc43e45dca9476e93ba48be
Amey Ambade <ameyambade@gmail.com>
2022-08-01 20:10:39 -0500

    Time: 78 ms (5.91%), Space: 14.2 MB (35.35%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,9 +1,8 @@
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsets1(self, nums: List[int]) -> List[List[int]]:
         # Choose a num or don't for a subset, so 2^n subsets. Each subset could be up to length n, so Time Complexity = O(n * 2^n)
         # Try building the tree for backtracking
-        res = []
-        
+        res = []        
         subset = []
         #i is the index of the value we are passing
         def dfs(i):
@@ -21,4 +20,22 @@ class Solution:
             dfs(i + 1)
             
         dfs(0)
+        return res
+    
+    # Backtracking traditional format
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # Choose a num or don't for a subset, so 2^n subsets. Each subset could be up to length n, so Time Complexity = O(n * 2^n)
+        # Try building the tree for backtracking
+        res = []    
+        subset = []
+        
+        def backtrack(start):
+            res.append(subset.copy())
+            for i in range(start, len(nums)):
+                subset.append(nums[i])
+                backtrack(i + 1)
+                subset.pop()
+        
+        backtrack(0)
+        
         return res
