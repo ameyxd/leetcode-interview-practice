5c75a5f727e95a8a3e183a5632e462570f2e54ab
Amey Ambade <ameyambade@gmail.com>
2022-08-01 19:44:25 -0500

    Time: 75 ms (74.70%), Space: 14.4 MB (43.94%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -2,41 +2,39 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         # Time complexity = O(N!) possible positions for the queens
         # Space complexity = O(n), since the maximum recursion depth is n, also the length for all three hashsets.
-
-
         ROWS, COLS = n, n
-        col = set()
+        cols = set()
         posDiag = set() # store (r + c)
         negDiag = set() # store (r - c)
         res = []
-        board = [["."] * COLS for i in range(ROWS)] 
+        board = [["."] * COLS for i in range(ROWS)]
         
         def backtrack(r):
             # base case: goal reached
             if r == ROWS:
                 # append board to result
-                copy = ["".join(row) for row in board]
+                copy = ["".join(rows) for rows in board]
                 res.append(copy)
                 return
             else:
                 for c in range(COLS):
                     # Validate
-                    if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    if c in cols or (r + c) in posDiag or (r - c) in negDiag:
                         continue
                     # Place
-                    col.add(c)
+                    cols.add(c)
                     posDiag.add(r + c)
                     negDiag.add(r - c)
                     board[r][c] = "Q"
-
-                    # Backtrack recurse
+                    
+                    # Backtrack
                     backtrack(r + 1)
-
+                    
                     # Unplace
-                    col.remove(c)
+                    cols.remove(c)
                     posDiag.remove(r + c)
                     negDiag.remove(r - c)
                     board[r][c] = "."
-            
+                    
         backtrack(0)
         return res
