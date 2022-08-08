347aa26051bf6415d233d960536166a060e62085
Amey Ambade <ameyambade@gmail.com>
2022-08-07 21:56:28 -0500

    Time: 41 ms (76.82%), Space: 13.9 MB (11.85%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -2,8 +2,8 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         carry = 0
         res = []
-        digits[-1] += 1
-        for i in range(len(digits) - 1, -1, -1):
+        digits[-1] += 1 # add 1 to last digit
+        for i in range(len(digits) - 1, -1, -1): # go in reverse order of digits and collect new number
             num = digits[i] + carry
             print(num)
             carry = num // 10
