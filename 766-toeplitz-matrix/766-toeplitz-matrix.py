d3c65d794025e4445c7030d2c4c225fcb0df2297
Amey Ambade <ameyambade@gmail.com>
2022-08-07 22:38:40 -0500

    Time: 84 ms (98.49%), Space: 14 MB (37.27%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.py
+++ 766-toeplitz-matrix/766-toeplitz-matrix.py
@@ -1,11 +1,12 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         ROWS, COLS = len(matrix), len(matrix[0])
+        # A one row or one col matrix is a Toeplitz matrix
         if ROWS == 1 or COLS == 1:
             return True
-        r, c = 1, 1
-        for row in range(r, ROWS):
-            for col in range(c, COLS):
+        # Start from second row second column
+        for row in range(1, ROWS):
+            for col in range(1, COLS):
                 if matrix[row][col] != matrix[row - 1][col - 1]:
                     return False
         return True
