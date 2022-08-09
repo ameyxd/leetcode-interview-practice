a12246dcaf69c7c00cc65da50f2ff2ae4c541799
Amey Ambade <ameyambade@gmail.com>
2022-08-08 23:45:18 -0500

    Time: 72 ms (60.77%), Space: 13.9 MB (37.22%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.py
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -1,37 +1,85 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
         ROWS, COLS = n, n
-        col = set()
-        posDiag = set() # (r + c)
-        negDiag = set() # (r - c)
-        # board = [[0] * ROWS for c in range(COLS)]
-        solutions = [0]
+        cols = set()
+        posDiag = set() # r + c
+        negDiag = set() # r - c
+        self.solutions = 0
         
         def backtrack(r):
-            # base case:
             if r == ROWS:
-                solutions[0] += 1
+                self.solutions += 1
                 return
             else:
                 for c in range(COLS):
-                    # validate
-                    if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    if c in cols or (r + c) in posDiag or (r - c) in negDiag:
                         continue
                     
-                    # place queen
-                    col.add(c)
-                    posDiag.add(r + c)
-                    negDiag.add(r - c)
-                    # board[r][c] = 1
+                    cols.add(c)
+                    posDiag.add((r + c))
+                    negDiag.add((r - c))
                     
-                    # backtrack
                     backtrack(r + 1)
                     
-                    # remove queen
-                    col.remove(c)
-                    posDiag.remove(r + c)
-                    negDiag.remove(r - c)
-                    # board[r][c] = 0
+                    cols.remove(c)
+                    posDiag.remove((r + c))
+                    negDiag.remove((r - c))
                     
         backtrack(0)
-        return solutions[0]
+        return self.solutions
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+#         ROWS, COLS = n, n
+#         col = set()
+#         posDiag = set() # (r + c)
+#         negDiag = set() # (r - c)
+#         # board = [[0] * ROWS for c in range(COLS)]
+#         solutions = [0]
+        
+#         def backtrack(r):
+#             # base case:
+#             if r == ROWS:
+#                 solutions[0] += 1
+#                 return
+#             else:
+#                 for c in range(COLS):
+#                     # validate
+#                     if c in col or (r + c) in posDiag or (r - c) in negDiag:
+#                         continue
+                    
+#                     # place queen
+#                     col.add(c)
+#                     posDiag.add(r + c)
+#                     negDiag.add(r - c)
+#                     # board[r][c] = 1
+                    
+#                     # backtrack
+#                     backtrack(r + 1)
+                    
+#                     # remove queen
+#                     col.remove(c)
+#                     posDiag.remove(r + c)
+#                     negDiag.remove(r - c)
+#                     # board[r][c] = 0
+                    
+#         backtrack(0)
+#         return solutions[0]
