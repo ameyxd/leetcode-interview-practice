9e9d3058db112ac684dfe359e08b6d6b98f20de1
Amey Ambade <ameyambade@gmail.com>
2022-08-11 18:13:23 -0500

    Time: 61 ms (38.19%), Space: 13.7 MB (98.34%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
+    def findTheDifference1(self, s: str, t: str) -> str:
         count_s, count_t = {}, {}
         for char in s:
             count_s[char] = count_s.get(char, 0) + 1
@@ -11,4 +11,16 @@ class Solution:
         for char, freq in count_t.items():
             if freq - count_s.get(char, 0) == 1:
                 return char
+
+    # O(1) space complexity
+    def findTheDifference(self, s: str, t: str) -> str:
+        char = 0
+        # XOR all chars in s
+        for c in s:
+            char ^= ord(c)
         
+        # XOR all chars in t
+        for c in t:
+            char ^= ord(c)
+        
+        return chr(char)
