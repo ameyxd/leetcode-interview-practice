3e5ad2f95ec0178b0d2aeb14540907c36e296ada
Amey Ambade <ameyambade@gmail.com>
2022-09-05 15:55:48 -0500

    Time: 935 ms (66.27%), Space: 54.6 MB (22.29%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -1,5 +1,6 @@
 class Solution:
-    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+    # BFS
+    def minTime1(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
         # Traverse in queue the nodes from apple to parent nodes, keep marking nodes that are in the path
         adj = defaultdict(list)
         for s, t in edges:
@@ -18,10 +19,37 @@ class Solution:
         while queue:
             node = queue.popleft()
             visited.add(node)
-            hasApple[node] = True
+            hasApple[node] = True # Mark nodes on path
 
             for par in adj[node]:
                 if par not in visited:
                     queue.append(par)
                     
         return sum(hasApple[1:]) * 2
+    
+    
+    # DFS
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        adj = defaultdict(list)
+        for s, t in edges:
+            adj[t].append(s)
+            adj[s].append(t)
+        visited = set()
+        
+        
+        def dfs(node):
+            time = 0
+            visited.add(node)
+            for child in adj[node]:
+                if child not in visited:
+                    time += dfs(child)
+            if (hasApple[node] or time):# and node != 0:
+                        return time + 2
+            return 0
+        
+        return max(dfs(0) - 2, 0) # Since we want to exclude the root node
+    
+    
+    
+    
+    
